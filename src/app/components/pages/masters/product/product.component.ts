067ed997d8ad4976b8fa7c2cdfbc0daf6ebb5191
Melissa Panduro <127220575+MelissaPanduro@users.noreply.github.com>
2025-04-02 02:57:18 +0000

    arreglos de campos

--- src/app/components/pages/masters/product/product.component.ts
+++ src/app/components/pages/masters/product/product.component.ts
@@ -22,6 +22,7 @@ export class ProductComponent implements OnInit {
   statusActive: boolean = true;
   isLoading: boolean = false;
 
+
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
